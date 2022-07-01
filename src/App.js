6901ef9c448d8a9e9120f36d245f42f8e13e1074
fatematuzjohora <fatematuzjohora889@gmail.com>
2022-07-01 12:51:49 +0600

    login page added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import "./App.css";
+import "react-toastify/dist/ReactToastify.css";
 import { Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
 import Header from "./Pages/Shared/Header";
 import BillingPage from "./Pages/BillingPage";
+import Login from "./Pages/Authenticatoin/Login";
+import { ToastContainer } from "react-toastify";
 
 function App() {
   return (
@@ -11,8 +14,10 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/home" element={<Home />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/billingPage" element={<BillingPage />} />
       </Routes>
+      <ToastContainer />
     </div>
   );
 }
--- src/Pages/Shared/Header.js
+++ src/Pages/Shared/Header.js
@@ -73,14 +73,14 @@ const Header = () => {
               </Link>
             </li>
           </ul>
-          <button
-            type="button"
+          <Link
+            to={"/login"}
             class="inline-block px-6 py-2.5 mr-2 bg-transparent text-blue-600 font-medium text-xs leading-tight uppercase rounded hover:text-blue-700 hover:bg-gray-100 focus:bg-gray-100 focus:outline-none focus:ring-0 active:bg-gray-200 transition duration-150 ease-in-out"
             data-mdb-ripple="true"
             data-mdb-ripple-color="light"
           >
             Login
-          </button>
+          </Link>
         </div>
       </div>
     </nav>
